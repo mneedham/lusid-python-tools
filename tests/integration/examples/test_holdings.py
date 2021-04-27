5d5b1237afe85eb4477ed6b353cef091f6428358
mneedham <mark.needham@finbourne.com>
2021-04-27 15:58:38 +0100

    more holdings

--- tests/integration/examples/test_holdings.py
+++ tests/integration/examples/test_holdings.py
@@ -44,7 +44,7 @@ class Holdings(unittest.TestCase):
         self.assertIsNotNone(portfolio_code)
 
         # tag::transactions-file[]
-        transactions_file = "data/test_transactions/transactions.csv"
+        transactions_file = "data/test_holdings/transactions.csv"
         # end::transactions-file[]
         transactions_file = Path(__file__).parent.joinpath(transactions_file)
 
@@ -63,10 +63,11 @@ class Holdings(unittest.TestCase):
             for _, instrument in instruments.iterrows()
         }
 
-        response = instruments_api.upsert_instruments(request_body=definitions)
+        holding_response = instruments_api.upsert_instruments(request_body=definitions)
         luid_to_name = {
-            response.values[key].to_dict()["lusid_instrument_id"]: response.values[key].to_dict()["name"]
-            for key in response.values
+            holding_response.values[key].to_dict()["lusid_instrument_id"]: holding_response.values[key].to_dict()[
+                "name"]
+            for key in holding_response.values
         }
         # end::import-instruments[]
 
@@ -93,17 +94,62 @@ class Holdings(unittest.TestCase):
 
             transaction_portfolios_api.upsert_transactions(
                 scope=scope, code=portfolio_code, transaction_request=transactions_request)
+
         # end::import-transactions[]
 
-        # tag::get-holdings[]
-        response = transaction_portfolios_api.get_holdings(scope=scope, code=portfolio_code)
-        holdings = pd.DataFrame([
-            {k: v for k, v in (value.to_dict().items())
-             if k in ["instrument_uid", "units", "cost"]}
-            for value in response.values
-        ])
-        holdings.insert(1, "instrument", holdings["instrument_uid"].apply(lambda x: luid_to_name.get(x, "Cash")))
-        holdings.loc[:, "cost"] = holdings["cost"].apply(lambda x: x["amount"])
-        # end::get-holdings[]
+        # tag::format-holdings[]
+        def display_holdings_summary(response):
+            return pd.DataFrame([{
+                "Instrument": value.properties["Instrument/default/Name"].value.label_value,
+                "Amount": value.cost.amount,
+                "Units": value.units
+            } for value in response.values])
+
+        # tag::format-holdings[]
+
+        # tag::get-holdings-funds-loaded[]
+        holding_response = transaction_portfolios_api.get_holdings(
+            scope=scope,
+            code=portfolio_code,
+            property_keys=["Instrument/default/Name"],
+            effective_at=datetime(year=2020, month=1, day=1, hour=1, tzinfo=pytz.UTC).isoformat(),
+        )
+        holdings = display_holdings_summary(holding_response)
+        # end::get-holdings-funds-loaded[]
+        self.write_to_test_output(holdings, "holdings_funds_loaded.csv")
+        self.assertEqual(holdings.shape[0], 1)
+
+        # tag::get-holdings-first-day-trading[]
+        holding_response = transaction_portfolios_api.get_holdings(
+            scope=scope,
+            code=portfolio_code,
+            property_keys=["Instrument/default/Name"],
+            effective_at=datetime(year=2020, month=1, day=2, hour=1, tzinfo=pytz.UTC).isoformat(),
+        )
+        holdings = display_holdings_summary(holding_response)
+        # end::get-holdings-first-day-trading[]
+        self.write_to_test_output(holdings, "holdings_first_day_trading.csv")
+        self.assertEqual(holdings.shape[0], 2)
+
+        # tag::get-holdings-second-day-trading[]
+        holding_response = transaction_portfolios_api.get_holdings(
+            scope=scope,
+            code=portfolio_code,
+            property_keys=["Instrument/default/Name"],
+            effective_at=datetime(year=2020, month=1, day=3, hour=1, tzinfo=pytz.UTC).isoformat(),
+        )
+        holdings = display_holdings_summary(holding_response)
+        # end::get-holdings-second-day-trading[]
+        self.write_to_test_output(holdings, "holdings_second_day_trading.csv")
+        self.assertEqual(holdings.shape[0], 4)
+
+        # tag::get-holdings-today[]
+        holding_response = transaction_portfolios_api.get_holdings(
+            scope=scope,
+            code=portfolio_code,
+            property_keys=["Instrument/default/Name"]
+        )
+        holdings = display_holdings_summary(holding_response)
+        # end::get-holdings-today[]
         self.write_to_test_output(holdings, "holdings.csv")
         self.assertEqual(holdings.shape[0], 4)
