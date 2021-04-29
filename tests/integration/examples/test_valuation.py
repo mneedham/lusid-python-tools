fbf2efee62cf690230146754e6088f7cb719b55d
Mark Needham <mark.needham@finbourne.com>
2021-04-29 14:18:28 +0100

    updates

--- tests/integration/examples/test_valuation.py
+++ tests/integration/examples/test_valuation.py
@@ -26,7 +26,6 @@ class Valuation(unittest.TestCase):
         transaction_portfolios_api = api_factory.build(lusid.api.TransactionPortfoliosApi)
         quotes_api = api_factory.build(lusid.api.QuotesApi)
         aggregation_api = api_factory.build(lusid.api.AggregationApi)
-        recipe_api = api_factory.build(lusid.api.ConfigurationRecipeApi)
         # end::apis[]
         portfolios_api = api_factory.build(lusid.api.PortfoliosApi)
 
@@ -47,7 +46,7 @@ class Valuation(unittest.TestCase):
         self.assertIsNotNone(portfolio_code)
 
         # tag::holdings-file[]
-        quotes_file = "data/test_quotes/holdings.csv"
+        quotes_file = "data/test_valuation/holdings.csv"
         # end::holdings-file[]
         quotes_file = Path(__file__).parent.joinpath(quotes_file)
 
@@ -90,7 +89,7 @@ class Valuation(unittest.TestCase):
         # end::set-holdings[]
 
         # tag::quotes-file[]
-        quotes_file = "data/test_quotes/quotes.csv"
+        quotes_file = "data/test_valuation/quotes.csv"
         # end::quotes-file[]
         quotes_file = Path(__file__).parent.joinpath(quotes_file)
 
@@ -120,58 +119,40 @@ class Valuation(unittest.TestCase):
         quotes_api.upsert_quotes(scope=scope, request_body=quotes_request)
         # end::import-quotes[]
 
+        # tag::compute-valuation[]
+        def compute_valuation_with_default_recipe(scope, code, date):
+            return aggregation_api.get_valuation(
+                valuation_request=models.ValuationRequest(
+                    recipe_id=models.ResourceId(scope=scope, code="default"),
+                    metrics=[
+                        models.AggregateSpec("Instrument/default/Name", "Value"),
+                        models.AggregateSpec("Holding/default/Units", "Sum"),
+                        models.AggregateSpec("Holding/default/PV", "Proportion"),
+                        models.AggregateSpec("Holding/default/PV", "Sum"),
+                    ],
+                    group_by=["Instrument/default/Name"],
+                    valuation_schedule=models.ValuationSchedule(effective_at=date),
+                    portfolio_entity_ids=[models.PortfolioEntityId(
+                        scope=scope,
+                        code=code,
+                        portfolio_entity_type="SinglePortfolio"
+                    )]
+                )
+            ).data
+        # end::compute-valuation[]
+
         # tag::get-valuation-20210421[]
         effective_at = datetime(year=2021, month=4, day=21, tzinfo=pytz.UTC)
-        response = aggregation_api.get_valuation(
-            valuation_request=models.ValuationRequest(
-                recipe_id=models.ResourceId(
-                    scope=scope,
-                    code="default"
-                ),
-                metrics=[
-                    models.AggregateSpec("Instrument/default/Name", "Value"),
-                    models.AggregateSpec("Holding/default/PV", "Proportion"),
-                    models.AggregateSpec("Holding/default/PV", "Sum"),
-                    models.AggregateSpec("Holding/default/Units", "Sum"),
-                ],
-                group_by=["Instrument/default/Name"],
-                valuation_schedule=models.ValuationSchedule(effective_at=effective_at),
-                portfolio_entity_ids=[models.PortfolioEntityId(
-                    scope=scope,
-                    code=portfolio_code,
-                    portfolio_entity_type="SinglePortfolio"
-                )]
-            )
-        )
-        valuation = pd.DataFrame([value for value in response.data])
+        response = compute_valuation_with_default_recipe(scope, portfolio_code, effective_at)
+        valuation = pd.DataFrame(response)
         # end::get-valuation-20210421[]
         self.write_to_test_output(valuation, "valuation-20210421.csv")
         self.assertAlmostEqual(valuation["Proportion(Holding/default/PV)"][0], 0.631707, 3)
 
         # tag::get-valuation-20210422[]
         effective_at = datetime(year=2021, month=4, day=22, tzinfo=pytz.UTC)
-        response = aggregation_api.get_valuation(
-            valuation_request=models.ValuationRequest(
-                recipe_id=models.ResourceId(
-                    scope=scope,
-                    code="default"
-                ),
-                metrics=[
-                    models.AggregateSpec("Instrument/default/Name", "Value"),
-                    models.AggregateSpec("Holding/default/PV", "Proportion"),
-                    models.AggregateSpec("Holding/default/PV", "Sum"),
-                    models.AggregateSpec("Holding/default/Units", "Sum"),
-                ],
-                group_by=["Instrument/default/Name"],
-                valuation_schedule=models.ValuationSchedule(effective_at=effective_at),
-                portfolio_entity_ids=[models.PortfolioEntityId(
-                    scope=scope,
-                    code=portfolio_code,
-                    portfolio_entity_type="SinglePortfolio"
-                )]
-            )
-        )
-        valuation = pd.DataFrame([value for value in response.data])
+        response = compute_valuation_with_default_recipe(scope, portfolio_code, effective_at)
+        valuation = pd.DataFrame(response)
         # end::get-valuation-20210422[]
         self.write_to_test_output(valuation, "valuation-20210422.csv")
         self.assertAlmostEqual(valuation["Proportion(Holding/default/PV)"][0], 0.6397, 3)
